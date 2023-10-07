2036201ce5363b7351babbbb712c220546c3764f
RajdeepDs <rajdeepds626@gmail.com>
2023-10-07 19:24:32 +0530

    test(unit): add unit tests for utils and validations

---
+++ backend/test/utils/generateTokens.test.ts
@@ -0,0 +1,46 @@
+import {
+  createAccessToken,
+  createRefreshToken,
+} from "../../src/utils/generateTokens";
+
+import jwt from "jsonwebtoken";
+
+jest.mock("jsonwebtoken");
+
+describe("Token Utilities", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("should create an access token with correct expiration", () => {
+    const userId = "sampleUserId";
+    const mockJwtSign = jwt.sign as jest.Mock;
+
+    mockJwtSign.mockReturnValue("sampleAccessToken");
+
+    const token = createAccessToken(userId);
+
+    expect(mockJwtSign).toHaveBeenCalledWith(
+      { userId },
+      process.env.ACCESS_TOKEN_SECRET!,
+      { expiresIn: "30m" }
+    );
+    expect(token).toBe("sampleAccessToken");
+  });
+
+  it("should create a refresh token with correct expiration", () => {
+    const userId = "sampleUserId";
+
+    const mockJwtSign = jwt.sign as jest.Mock;
+
+    mockJwtSign.mockReturnValue("sampleRefreshToken");
+
+    const token = createRefreshToken(userId);
+
+    expect(mockJwtSign).toHaveBeenCalledWith(
+      { userId },
+      process.env.REFRESH_TOKEN_SECRET!,
+      { expiresIn: "7d" }
+    );
+  });
+});
---
+++ backend/test/utils/getUserFromToken.test.ts
@@ -0,0 +1,39 @@
+import { getUser } from "../../src/utils/getUserFromToken";
+import jwt from "jsonwebtoken";
+
+jest.mock("jsonwebtoken");
+
+describe("Get user from Token Utilities", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+  it("should return null if no token is provided", async () => {
+    const mockJwtVerify = jwt.verify as jest.Mock;
+
+    mockJwtVerify.mockReturnValue("sampleDecodedToken");
+
+    const user = await getUser("");
+
+    expect(user).toBe(null);
+  });
+
+  it("should return null if token is invalid", async () => {
+    const mockJwtVerify = jwt.verify as jest.Mock;
+
+    mockJwtVerify.mockReturnValue("sampleDecodedToken");
+
+    const user = await getUser("sampleToken");
+
+    expect(user).toBe("sampleDecodedToken");
+  });
+
+  it("should return decoded token if token is valid", async () => {
+    const mockJwtVerify = jwt.verify as jest.Mock;
+
+    mockJwtVerify.mockReturnValue("sampleDecodedToken");
+
+    const user = await getUser("Bearer sampleToken");
+
+    expect(user).toBe("sampleDecodedToken");
+  });
+});
