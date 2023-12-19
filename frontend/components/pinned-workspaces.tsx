3b43d238d533d2b36074703337a7e7f20a0dc60b
RajdeepDs <rajdeepds626@gmail.com>
2023-12-19 19:30:12 +0530

    feat(dashboard): add draggable workspace cards

--- frontend/components/pinned-workspaces.tsx
+++ frontend/components/pinned-workspaces.tsx
@@ -1,54 +1,53 @@
-import { Icons } from "./icon";
+"use client";
+import { use, useState } from "react";
+import PinnedWorkspaceCard from "./pinned-workspace-card";
+
+import { DndContext, closestCenter } from "@dnd-kit/core";
+import { SortableContext, rectSortingStrategy } from "@dnd-kit/sortable";
 
 export default function PinnedWorkspaces() {
+  const data = [
+    {
+      id: 1,
+      title: "Luminate",
+      description: "A cloud based IDE.",
+      language: "TypeScript",
+      stars: 2,
+    },
+    {
+      id: 2,
+      title: "NoteUp",
+      description: "A note taking web app.",
+      language: "TypeScript",
+      stars: 1,
+    },
+  ];
+  const [users, setUsers] = useState(data);
+  const onDragEnd = (event: any) => {
+    const { active, over } = event;
+    if (active.id === over.id) {
+      return;
+    }
+    setUsers((users) => {
+      const oldIndex = users.findIndex((user) => user.id === active.id);
+      const newIndex = users.findIndex((user) => user.id === over.id);
+      const newUsers = [...users];
+      newUsers.splice(oldIndex, 1);
+      newUsers.splice(newIndex, 0, users[oldIndex]);
+      return newUsers;
+    });
+  };
   return (
     <div className="">
       <h1 className="text-lg font-semibold">Pinned Workspaces</h1>
       <div className="mt-4 grid grid-cols-2 gap-4">
-        <div className="border-blueGray to-blueGray/30 w-full rounded-lg border bg-gradient-to-br from-gray/30 from-20% to-80% p-5">
-          <div className="flex items-center justify-between">
-            <div className=" flex items-center gap-2">
-              <Icons.box className="h-4 w-4" />
-              <h1 className="cursor-pointer text-base hover:underline">
-                NoteUp
-              </h1>
-            </div>
-            <Icons.gripVertical className="h-4 w-4 cursor-grab" />
-          </div>
-          <p className="mt-1 font-extralight">A note taking web app.</p>
-          <div className="mt-5 flex items-center space-x-4">
-            <div className="flex items-center gap-2">
-              <div className="h-3 w-3 rounded-full bg-blue-600" />
-              <p className="text-sm font-extralight">TypeScript</p>
-            </div>
-            <div className="flex items-center gap-1">
-              <Icons.star className="h-4 w-4" />
-              <span className="text-sm font-extralight">2</span>
-            </div>
-          </div>
-        </div>
-        <div className="border-blueGray to-blueGray/30 w-full rounded-lg border bg-gradient-to-br from-gray/30 from-20% to-80% p-5">
-          <div className="flex items-center justify-between">
-            <div className=" flex items-center gap-2">
-              <Icons.box className="h-4 w-4" />
-              <h1 className="cursor-pointer text-base hover:underline">
-                NoteUp
-              </h1>
-            </div>
-            <Icons.gripVertical className="h-4 w-4 cursor-grab" />
-          </div>
-          <p className="mt-1 font-extralight">A note taking web app.</p>
-          <div className="mt-5 flex items-center space-x-4">
-            <div className="flex items-center gap-2">
-              <div className="h-3 w-3 rounded-full bg-blue-600" />
-              <p className="text-sm font-extralight">TypeScript</p>
-            </div>
-            <div className="flex items-center gap-1">
-              <Icons.star className="h-4 w-4" />
-              <span className="text-sm font-extralight">2</span>
-            </div>
-          </div>
-        </div>
+        <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd}>
+          <SortableContext items={users} strategy={rectSortingStrategy}>
+            {users.map((user) => (
+              <PinnedWorkspaceCard key={user.id} user={user} />
+            ))}
+          </SortableContext>
+        </DndContext>
       </div>
     </div>
   );
