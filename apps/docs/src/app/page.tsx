c9c8d51acbd751952873d2c88d93237a138f2a9c
RajdeepDs <rajdeepds626@gmail.com>
2024-01-08 20:58:26 +0530

    delete docs app

--- apps/docs/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@repo/ui";
-
-export default function Page(): JSX.Element {
-  return (
-    <main className="">
-      <Button className="ml-5">Click Me</Button>
-    </main>
-  );
-}
