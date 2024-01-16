f23abaeb8e4d8caf0b71361d7536a4865c9a503a
RajdeepDs <rajdeepds626@gmail.com>
2024-01-16 20:51:23 +0530

    feat: add credentials provider

--- apps/web/src/auth.config.ts
+++ apps/web/src/auth.config.ts
@@ -1,7 +1,7 @@
-import GitHub from "next-auth/providers/github";
+import credentials from "next-auth/providers/credentials";
 
 import type { NextAuthConfig } from "next-auth";
 
 export default {
-  providers: [GitHub],
+  providers: [credentials],
 } satisfies NextAuthConfig;
